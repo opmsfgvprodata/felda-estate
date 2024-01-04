bb6a0e45130004ee62e0a666cf86c475353be0f9
Fatin <fatin@alfatechsolution.com>
2024-01-04 11:12:54 +0800

    update dashboard status akaun

--- MVC_SYSTEM/Controllers/DashboardController.cs
+++ MVC_SYSTEM/Controllers/DashboardController.cs
@@ -52,8 +52,16 @@ namespace MVC_SYSTEM.Controllers
 
             GetNSWL.GetSyarikat(out SyarikatID, getuserid, User.Identity.Name);
 
-            int month = DateTime.Now.Month - 1;
-            int year = DateTime.Now.Year;
+            //original code
+            //int month = DateTime.Now.Month - 1;
+            //int year = DateTime.Now.Year;
+
+            //fatin modified - 03/01/2024
+            DateTime currentDate = DateTime.Now;
+            DateTime previousMonthDate = currentDate.AddMonths(-1);
+            int month = previousMonthDate.Month;
+            int year = (currentDate.Month == 1) ? currentDate.Year - 1 : currentDate.Year;
+
 
             List<sp_DashStatusAkaun_Result> dashStatusAkaun = new List<sp_DashStatusAkaun_Result>();
 
